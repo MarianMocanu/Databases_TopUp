c906785841a4866a919e3a2dd46f565ae95943b9
Marian Mocanu <gheo0174@stud.kea.dk>
2021-02-07 13:49:20 +0100

    configuration complete

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
-const app = require('express')();
 
 
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
 
 
-
-app.get('/', (req, res, ) => { 
-    res.send('hello');
+app.use(cors());
+// app.use(express.static(path.join(__dirname, '../frontend')));
+app.get('/', (req, res) => res.send({ text: 'Default page' }));
+app.get('/hello', (req, res) => {
+    console.log('wadfghsdfh');
+    return res.send({ message: 'hello' });
 });
 
+app.get()
 
 const port = process.env.PORT ? process.env.PORT : 3001;
-const server = app.listen(port, error => { 
+const server = app.listen(port, error => {
     if (error) {
         console.log('Error starting the server!');
     };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+
+    fetch('/hello').then(res => console.log(res.text()));
+
+
     return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
